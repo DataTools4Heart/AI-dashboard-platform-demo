e86e0c799a11f7c262ad9caa2fb7d579f2c9d3bc
Josep Ll. Gelpi <gelpi@ub.edu>
2023-09-30 09:20:33 +0200

    Added tools folders

---
+++ tools/dataDiscovery/site_discovery.py
@@ -0,0 +1,20 @@
+''' DT4H Site Status. Obtain full technical status of DT4H sites
+'''
+import argparse
+import json
+
+parser = argparse.ArgumentParser(
+    prog="site_status",
+    description="retrieve status of a site"
+)
+
+parser.add_argument('--config', type=open, help="DT4H configuration", required=True)
+parser.add_argument('--output_path', action='store', help="Output file", required=True)
+
+args = parser.parse_args()
+
+config = json.load(args.config)
+
+#TODO
+with open(args.output_path, "w") as output_file:
+    json.dump(config, output_file)
