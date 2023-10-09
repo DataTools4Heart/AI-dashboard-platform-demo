09415394f892f2f8ea3d875b3cc8eb089ece3011
Josep Ll. Gelpi <gelpi@ub.edu>
2023-10-09 10:04:33 +0200

    Added userdata initialization to mongo_seed

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git
-git clone --single-branch --branch dt4h_gui https://gitlab.bsc.es/inb/dt4h/openVRE.git
+apt-get install -y git rsync
+#git clone https://gitlab.bsc.es/inb/dt4h/openVRE.git
 #for f in openVRE/install/database/*.json; do mongoimport --host my-mongodb --db open-vre --collection vre-collection -file /mongodb/mongo-init.js -u ${MONGO_ADMIN} -p ${MONGO_ADMIN_PASS} --authenticationDatabase admin $f; done
-for f in openVRE/install/database/*.json; do mongoimport --host my-mongodb --db openVRE --drop -u 'admin' -p 'admin' --authenticationDatabase admin $f; done
+for f in /install_data/database/*.json; do mongoimport --host my-mongodb --db openVRE --drop -u 'admin' -p 'admin' --authenticationDatabase admin $f; done
+rsync -av --delete /install_data/data/userdata /shared_data
+
