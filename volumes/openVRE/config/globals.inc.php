0c471afa8b66a744f4465ed878c32d50762148f6
Josep Ll. Gelpi <gelpi@ub.edu>
2023-10-09 13:01:04 +0200

    Added ref to actual host path for proper mounting, minor fixes, logs moved to shared_data

--- volumes/openVRE/config/globals.inc.php
+++ volumes/openVRE/config/globals.inc.php
@@ -7,7 +7,7 @@
 
 // Main config
 #$GLOBALS['SERVER']    = "http://".$_SERVER['HTTP_HOST']; // domain
-$GLOBALS['SERVER']    = "http://jlgclnx:8089"; // domain
+$GLOBALS['SERVER']    = "http://jlgbio2:8089"; // domain
 $GLOBALS['BASEURL']   = "/"; // prefix url path. Set "/" for no prefix
 $GLOBALS['AppPrefix'] = "EUCAIM"; // project url acronym
 $GLOBALS['NAME']      = "European Cancer Images FLD"; // project name
@@ -28,11 +28,11 @@ $GLOBALS['dbname_VRE']     = "openVRE"; // Database name
 
 //VRE installation paths
 $GLOBALS['root']       = dirname(__DIR__); // VRE root directory
-$GLOBALS['logFile']    = $GLOBALS['root']."/logs/application.log"; // Log file path
+$GLOBALS['logFile']    = $GLOBALS['shared']."/logs/application.log"; // Log file path
 $GLOBALS['shared']     = "/shared_data/"; // VRE data directory
-$GLOBALS['dataDir']    = $GLOBALS['shared']."userdata/"; // User data directory
-$GLOBALS['pubDir']     = $GLOBALS['shared']."public/"; // Public data directory
-$GLOBALS['sampleData'] = $GLOBALS['shared']."sampleData/"; // Tool dataset directory
+$GLOBALS['dataDir']    = $GLOBALS['shared']."userdata"; // User data directory
+$GLOBALS['pubDir']     = $GLOBALS['shared']."public"; // Public data directory
+$GLOBALS['sampleData'] = $GLOBALS['shared']."sampleData"; // Tool dataset directory
 $GLOBALS['sampleData_default'] = "basic"; // Default workspace's dataset entry
 
 // File manager config
@@ -55,7 +55,7 @@ $GLOBALS['auth_credentials']       = __DIR__."/oauth2.conf"; // oauth2 client cr
 $GLOBALS['authAdmin_credentials']  = __DIR__."/oauth2_admin.conf"; // oauth2 client credentials with admin privileges
 #$GLOBALS['authServer']             = 'http://84.88.186.195:8089/auth'; // external oauth2 server
 #$GLOBALS['authServer']             = parse_url($_SERVER['HTTP_HOST'], PHP_URL_HOST).':8080/auth/'; // internal oauth2 server
-$GLOBALS['authServer']             = 'http://jlgclnx:8080/auth'; // internal oauth2 server
+$GLOBALS['authServer']             = 'http://jlgbio2:8080/auth'; // internal oauth2 server
 $GLOBALS['authRealm']              = 'open-vre'; // keycloak realm
 $GLOBALS['urlAuthorize' ]          = $GLOBALS['authServer'].'/realms/'.$GLOBALS['authRealm'].'/protocol/openid-connect/auth';     //get autorization_code
 $GLOBALS['urlAccessToken']         = $GLOBALS['authServer'].'/realms/'.$GLOBALS['authRealm'].'/protocol/openid-connect/token';    //get token
@@ -132,7 +132,7 @@ $GLOBALS['placeholder_textarea'] = "Click right button to select file(s)"; // te
 
 
 // Cloud infrastructures
-$GLOBALS['cloud']              = "my_on_premises_cloud"; // VRE central cloud. Options are any of $GLOBALS['clouds']
+$GLOBALS['cloud']              = "my_on_premises_docker"; // VRE central cloud. Options are any of $GLOBALS['clouds']
 $GLOBALS['clouds'] = Array(
 		'my_on_premises_cloud' => array(
 			"http_host"	    => "www.mydomain.com",	     // used in getCurrentCloud
@@ -146,39 +146,19 @@ $GLOBALS['clouds'] = Array(
 			"imageTypes" 	    => array(),                      // list of cloud OCCI templates indexed by RAM (GB)
 			"auth"	            => array("required" => False)
 		),
-
-		'my_remote_2' => array(
-			"http_host"	    => "www.mydomain2.com",	      // used in getCurrentCloud
-			"dataDir_fs"   	    => "/NFS/export/path2/userdata/", // export path for NFS server
-			"pubDir_fs"	    => "/NFS/export/path2/public/",   // export path for NFS server
-			"dataDir_virtual"   => "/shared/path/for/userdata",
-			"pubDir_virtual"    => "/shared/path/for/public",
-			"PMESserver_domain" => "pmes.mydomain2.com",
-			"PMESserver_port"   => "8080",
+		'my_on_premises_docker' => array(
+			"http_host"	    => "localhost",	     // used in getCurrentCloud
+			"dataDir_fs"        => "/NFS/export/path/userdata/", // export path for NFS server
+			"pubDir_fs"         => "/NFS/export/path/public/",   // export path for NFS server
+			"dataDir_virtual"   => $GLOBALS['dataDir']."_tmp",
+			"pubDir_virtual"    => $GLOBALS['pubDir']."_tmp",
+			"PMESserver_domain" => "pmes.mydomain.com",
+			"PMESserver_port"   => "80",
 			"PMESserver_address"=> "/",
-			"imageTypes"	=>  array(                           // list of cloud OCCI templates indexed by RAM (GB)
-				"2"  => array(
-					"1"  => array("id" => "small", "disk" => null, "name" => "small"),
-					"8"  => array("id" => "large-small", "disk" => null, "name" => "large-small"),
-					"16" => array("id" => "extra_large-small", "disk" => null, "name" => "large-small")
-				    ),
-				"4"  => array(
-					"2"  => array("id" => "medium", "disk" => null, "name" => "medium"),
-					"4"  => array("id" => "medium-medium", "disk" => null, "name" => "medium-medium"),
-					"8"  => array("id" => "large-medium", "disk" => null, "name" => "large-medium"),
-					"16" => array("id" => "extra_large-medium", "disk" => null, "name" => "extra_large-medium")
-				    )
-			),
-			"auth"  => array(
-				"required"       => True,
-				"OS_NO_CACHE"    => "True",
-				"OS_CLOUDNAME"   => "overcloud",
-				"OS_AUTH_URL"    => "https://extcloud05.ebi.ac.uk:13000/v2.0",
-				"NOVA_VERSION"   => "1.2",
-				"COMPUTE_API_VERSION" => "1.2",
-				"OS_USERNAME"    => "username@mail.es",
-				"OS_PASSWORD"    => "s3cr3t",
-				"OS_TENANT_NAME" => "tenancy_name"
-			)
-	    )
+			"imageTypes" 	    => array(),                      // list of cloud OCCI templates indexed by RAM (GB)
+			"auth"	            => array("required" => False)
+		)
 );
+
+# Get host path
+$GLOBALS['host_path'] = trim(file_get_contents($GLOBALS['shared'].".pwd"));
